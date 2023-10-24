d97a4c3407f3668ab77f550ca3eedf4eb5532e2f
Xavier Mitault <xavier.mitault@epitech.eu>
2023-10-24 23:11:09 +0000

    CICD: Fix scripts release
    PATCH

--- scripts/create-tar.sh
+++ scripts/create-tar.sh
@@ -1,7 +1,9 @@
-NOW=$(pwd)
+mkdir b-luga
 
-cd ..
+mv ./CMakeLists.txt ./b-luga
+mv ./LICENSE ./b-luga
+mv ./README.md ./b-luga
+mv ./include ./b-luga
+mv ./deps ./b-luga
 
-tar -cvf b-luga.tar $NOW
-
-cd "$NOW"
+tar -cvf b-luga.tar b-luga
