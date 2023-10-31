7e98e9f6d3fbff000cb86bf2fad615899191f757
Xavier Mitault <xavier.mitault@epitech.eu>
2023-10-31 20:34:12 +0000

    ECS: Remove try test

--- libs/B-luga-graphics/src/test.cpp
+++ libs/B-luga-graphics/src/test.cpp
@@ -1,12 +1,3 @@
-#include "B-luga-graphics/AnimRect.hpp"
-#include "B-luga-graphics/AudioSystems.hpp"
-#include "B-luga-graphics/GraphicsCustomTypes.hpp"
-#include "B-luga-graphics/GraphicsSystems.hpp"
-#include "B-luga-graphics/ParallaxSystems.hpp"
-#include "B-luga-graphics/SharedValues.hpp"
-#include "B-luga-graphics/SpriteSystems.hpp"
-#include "B-luga-graphics/TextSystems.hpp"
-
 namespace BLugaGraphics {
     void exist() {
         return;
