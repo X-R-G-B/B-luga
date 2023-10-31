8922221b6a83a984f0d9edd9b3d7c264dbba9afc
Xavier Mitault <xavier.mitault@epitech.eu>
2023-10-31 15:17:18 +0000

    ECS: Fix plugin way

---
+++ libs/B-luga/test/main.cpp
@@ -0,0 +1,16 @@
+#include "B-luga/Maths/Maths.hpp"
+
+#include "B-luga/Clock.hpp"
+#include "B-luga/IPlugin.hpp"
+#include "B-luga/Json.hpp"
+#include "B-luga/Logger.hpp"
+#include "B-luga/PluginHandler.hpp"
+#include "B-luga/Registry.hpp"
+#include "B-luga/SceneManager.hpp"
+#include "B-luga/SparseArray.hpp"
+
+int main() {
+    Clock clock;
+    clock.create();
+    Logger::debug("bite");
+}
