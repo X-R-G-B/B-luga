5d6bf72ea85bea2e6977800ba91976e39bf75d0e
Xavier Mitault <xavier.mitault@epitech.eu>
2023-11-01 16:06:31 +0000

    BLUGA-GRAPHICS: Fix src

--- libs/B-luga-graphics/scripts/create-tar.sh
+++ libs/B-luga-graphics/scripts/create-tar.sh
@@ -6,5 +6,6 @@ mv ./CMakeLists.txt ./b-luga-graphics
 mv ./README.md ./b-luga-graphics
 mv ./deps/ ./b-luga-graphics
 mv ./include/ ./b-luga-graphics
+mv ./src ./b-luga-graphics
 
 tar -cvf b-luga-graphics.tar b-luga-graphics
