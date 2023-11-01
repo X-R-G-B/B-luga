0649bd162c595df3e89732f8b10ff223f684e109
Xavier Mitault <xavier.mitault@epitech.eu>
2023-11-01 18:32:02 +0000

    BLUGA-GRAPHICS: Remove

--- libs/B-luga-graphics/src/RaylibImpl/Audio/Audio.cpp
+++ libs/B-luga-graphics/src/RaylibImpl/Audio/Audio.cpp
@@ -22,67 +22,67 @@ namespace Raylib {
                 SetSoundVolume(_sound, volume);
             }
 
-            bool SoundImpl::isReady() const override
+            bool SoundImpl::isReady() const
             {
                 return IsSoundReady(_sound);
             }
 
-            void SoundImpl::unload() override
+            void SoundImpl::unload()
             {
                 UnloadSound(_sound);
             }
 
-            void SoundImpl::play() const override
+            void SoundImpl::play() const
             {
                 PlaySound(_sound);
             }
 
-            void SoundImpl::stop() const override
+            void SoundImpl::stop() const
             {
                 StopSound(_sound);
             }
 
-            void SoundImpl::pause() const override
+            void SoundImpl::pause() const
             {
                 PauseSound(_sound);
             }
 
-            void SoundImpl::resume() const override
+            void SoundImpl::resume() const
             {
                 ResumeSound(_sound);
             }
 
-            bool SoundImpl::isPlaying() const override
+            bool SoundImpl::isPlaying() const
             {
                 return IsSoundPlaying(_sound);
             }
 
-            void SoundImpl::setVolume(float volume) const override
+            void SoundImpl::setVolume(float volume) const
             {
                 SetSoundVolume(_sound, volume);
             }
 
-            void SoundImpl::setPitch(float pitch) const override
+            void SoundImpl::setPitch(float pitch) const
             {
                 SetSoundPitch(_sound, pitch);
             }
 
-            void SoundImpl::setPan(float pan) const override
+            void SoundImpl::setPan(float pan) const
             {
                 SetSoundPitch(_sound, pan);
             }
 
-            bool SoundImpl::NeedToPlay() const override
+            bool SoundImpl::NeedToPlay() const
             {
                 return _needToPlay;
             }
 
-            void SoundImpl::setNeedToPlay(bool needToPlay) override
+            void SoundImpl::setNeedToPlay(bool needToPlay)
             {
                 _needToPlay = needToPlay;
             }
 
-            const std::string &SoundImpl::getPath() const override
+            const std::string &SoundImpl::getPath() const
             {
                 return _path;
             }
@@ -94,82 +94,82 @@ namespace Raylib {
                 SetMusicVolume(_music, volume);
             }
 
-            void MusicImpl::unload() override
+            void MusicImpl::unload()
             {
                 UnloadMusicStream(_music);
             }
 
-            bool MusicImpl::isReady() const override
+            bool MusicImpl::isReady() const
             {
                 return IsMusicReady(_music);
             }
 
-            void MusicImpl::play() const override
+            void MusicImpl::play() const
             {
                 PlayMusicStream(_music);
             }
 
-            bool MusicImpl::isPlaying() const override
+            bool MusicImpl::isPlaying() const
             {
                 return IsMusicStreamPlaying(_music);
             }
 
-            void MusicImpl::update() const override
+            void MusicImpl::update() const
             {
                 UpdateMusicStream(_music);
             }
 
-            void MusicImpl::stop() const override
+            void MusicImpl::stop() const
             {
                 StopMusicStream(_music);
             }
 
-            void MusicImpl::pause() const override
+            void MusicImpl::pause() const
             {
                 PauseMusicStream(_music);
             }
 
-            void MusicImpl::resume() const override
+            void MusicImpl::resume() const
             {
                 ResumeMusicStream(_music);
             }
 
-            void MusicImpl::setVolume(float volume) const override
+            void MusicImpl::setVolume(float volume) const
             {
                 SetMusicVolume(_music, volume);
             }
 
-            void MusicImpl::setPitch(float pitch) const override
+            void MusicImpl::setPitch(float pitch) const
             {
                 SetMusicPitch(_music, pitch);
             }
 
-            void MusicImpl::setPan(float pan) const override
+            void MusicImpl::setPan(float pan) const
             {
                 SetMusicPitch(_music, pan);
             }
 
-            float MusicImpl::getTimeLength() const override
+            float MusicImpl::getTimeLength() const
             {
                 return GetMusicTimeLength(_music);
             }
 
-            float MusicImpl::getTimePlayed() const override
+            float MusicImpl::getTimePlayed() const
             {
                 return GetMusicTimePlayed(_music);
             }
 
-            bool MusicImpl::NeedToPlay() const override
+            bool MusicImpl::NeedToPlay() const
             {
                 return _needToPlay;
             }
 
-            void MusicImpl::setNeedToPlay(bool needToPlay) override
+            void MusicImpl::setNeedToPlay(bool needToPlay)
             {
                 _needToPlay = needToPlay;
             }
 
-            const std::string &MusicImpl::getPath() const override
+            const std::string &MusicImpl::getPath() const
             {
                 return _path;
             }
