0aefd66abcc69a64dbed12338b9a59d1dc6ceb50
Xavier Mitault <xavier.mitault@epitech.eu>
2023-11-04 11:56:16 +0000

    BLUGA-GRAPHICS: Fix color

--- libs/B-luga-graphics/include/B-luga-graphics/Raylib/Geometry/Geometry.cpp
+++ libs/B-luga-graphics/include/B-luga-graphics/Raylib/Geometry/Geometry.cpp
@@ -4,28 +4,28 @@ extern "C" {
 #include "Geometry.hpp"
 
 namespace Raylib {
-    const Color DarkGray   = Raylib::Color::Color(80, 80, 80, 255);
-    const Color Yellow     = Raylib::Color::Color(253, 249, 0, 255);
-    const Color Gold       = Raylib::Color::Color(255, 203, 0, 255);
-    const Color Orange     = Raylib::Color::Color(255, 161, 0, 255);
-    const Color Pink       = Raylib::Color::Color(255, 109, 194, 255);
-    const Color Red        = Raylib::Color::Color(230, 41, 55, 255);
-    const Color Maroon     = Raylib::Color::Color(190, 33, 55, 255);
-    const Color Green      = Raylib::Color::Color(0, 228, 48, 255);
-    const Color Lime       = Raylib::Color::Color(0, 158, 47, 255);
-    const Color DarkGreen  = Raylib::Color::Color(0, 117, 44, 255);
-    const Color SkyBlue    = Raylib::Color::Color(102, 191, 255, 255);
-    const Color Blue       = Raylib::Color::Color(0, 121, 241, 255);
-    const Color DarkBlue   = Raylib::Color::Color(0, 82, 172, 255);
-    const Color Purple     = Raylib::Color::Color(200, 122, 255, 255);
-    const Color Violet     = Raylib::Color::Color(135, 60, 190, 255);
-    const Color DarkPurple = Raylib::Color::Color(112, 31, 126, 255);
-    const Color Beige      = Raylib::Color::Color(211, 176, 131, 255);
-    const Color Brown      = Raylib::Color::Color(127, 106, 79, 255);
-    const Color DarkBrown  = Raylib::Color::Color(76, 63, 47, 255);
-    const Color White      = Raylib::Color::Color(255, 255, 255, 255);
-    const Color Black      = Raylib::Color::Color(0, 0, 0, 255);
-    const Color Blank      = Raylib::Color::Color(0, 0, 0, 0);
-    const Color Magenta    = Raylib::Color::Color(255, 0, 255, 255);
-    const Color RayWhite   = Raylib::Color::Color(245, 245, 245, 255);
+    const Color DarkGray   = Raylib::Color(80, 80, 80, 255);
+    const Color Yellow     = Raylib::Color(253, 249, 0, 255);
+    const Color Gold       = Raylib::Color(255, 203, 0, 255);
+    const Color Orange     = Raylib::Color(255, 161, 0, 255);
+    const Color Pink       = Raylib::Color(255, 109, 194, 255);
+    const Color Red        = Raylib::Color(230, 41, 55, 255);
+    const Color Maroon     = Raylib::Color(190, 33, 55, 255);
+    const Color Green      = Raylib::Color(0, 228, 48, 255);
+    const Color Lime       = Raylib::Color(0, 158, 47, 255);
+    const Color DarkGreen  = Raylib::Color(0, 117, 44, 255);
+    const Color SkyBlue    = Raylib::Color(102, 191, 255, 255);
+    const Color Blue       = Raylib::Color(0, 121, 241, 255);
+    const Color DarkBlue   = Raylib::Color(0, 82, 172, 255);
+    const Color Purple     = Raylib::Color(200, 122, 255, 255);
+    const Color Violet     = Raylib::Color(135, 60, 190, 255);
+    const Color DarkPurple = Raylib::Color(112, 31, 126, 255);
+    const Color Beige      = Raylib::Color(211, 176, 131, 255);
+    const Color Brown      = Raylib::Color(127, 106, 79, 255);
+    const Color DarkBrown  = Raylib::Color(76, 63, 47, 255);
+    const Color White      = Raylib::Color(255, 255, 255, 255);
+    const Color Black      = Raylib::Color(0, 0, 0, 255);
+    const Color Blank      = Raylib::Color(0, 0, 0, 0);
+    const Color Magenta    = Raylib::Color(255, 0, 255, 255);
+    const Color RayWhite   = Raylib::Color(245, 245, 245, 255);
 }
