7fbafdeb35517b0124629eb0d1cb9cc278208084
Xavier Mitault <xavier.mitault@epitech.eu>
2023-11-04 12:24:43 +0000

    BLUGA-GRAPHICS: Fix color

--- libs/B-luga-graphics/include/B-luga-graphics/Raylib/Geometry/Geometry.cpp
+++ libs/B-luga-graphics/include/B-luga-graphics/Raylib/Geometry/Geometry.cpp
@@ -4,35 +4,45 @@ extern "C" {
 #include "Geometry.hpp"
 
 namespace Raylib {
-    Color::Color(ColorDef color)
+    Color::Color(ColorDef colorDef)
     {
-        switch (color) {
-            case ColorDef::DarkGray   : return Raylib::Color(80, 80, 80, 255);
-            case ColorDef::Yellow     : return Raylib::Color(253, 249, 0, 255);
-            case ColorDef::Gold       : return Raylib::Color(255, 203, 0, 255);
-            case ColorDef::Orange     : return Raylib::Color(255, 161, 0, 255);
-            case ColorDef::Pink       : return Raylib::Color(255, 109, 194, 255);
-            case ColorDef::Red        : return Raylib::Color(230, 41, 55, 255);
-            case ColorDef::Maroon     : return Raylib::Color(190, 33, 55, 255);
-            case ColorDef::Green      : return Raylib::Color(0, 228, 48, 255);
-            case ColorDef::Lime       : return Raylib::Color(0, 158, 47, 255);
-            case ColorDef::DarkGreen  : return Raylib::Color(0, 117, 44, 255);
-            case ColorDef::SkyBlue    : return Raylib::Color(102, 191, 255, 255);
-            case ColorDef::Blue       : return Raylib::Color(0, 121, 241, 255);
-            case ColorDef::DarkBlue   : return Raylib::Color(0, 82, 172, 255);
-            case ColorDef::Purple     : return Raylib::Color(200, 122, 255, 255);
-            case ColorDef::Violet     : return Raylib::Color(135, 60, 190, 255);
-            case ColorDef::DarkPurple : return Raylib::Color(112, 31, 126, 255);
-            case ColorDef::Beige      : return Raylib::Color(211, 176, 131, 255);
-            case ColorDef::Brown      : return Raylib::Color(127, 106, 79, 255);
-            case ColorDef::DarkBrown  : return Raylib::Color(76, 63, 47, 255);
-            case ColorDef::White      : return Raylib::Color(255, 255, 255, 255);
-            case ColorDef::Black      : return Raylib::Color(0, 0, 0, 255);
-            case ColorDef::Blank      : return Raylib::Color(0, 0, 0, 0);
-            case ColorDef::Magenta    : return Raylib::Color(255, 0, 255, 255);
-            case ColorDef::RayWhite   : return Raylib::Color(245, 245, 245, 255);
-            default                : return Raylib::Color(0, 0, 0, 0);
+        Color color(0, 0, 0, 0);
+
+        switch (colorDef) {
+            case ColorDef::DarkGray   : *this = Raylib::Color(80, 80, 80, 255);
+            case ColorDef::Yellow     : *this = Raylib::Color(253, 249, 0, 255);
+            case ColorDef::Gold       : *this = Raylib::Color(255, 203, 0, 255);
+            case ColorDef::Orange     : *this = Raylib::Color(255, 161, 0, 255);
+            case ColorDef::Pink       : *this = Raylib::Color(255, 109, 194, 255);
+            case ColorDef::Red        : *this = Raylib::Color(230, 41, 55, 255);
+            case ColorDef::Maroon     : *this = Raylib::Color(190, 33, 55, 255);
+            case ColorDef::Green      : *this = Raylib::Color(0, 228, 48, 255);
+            case ColorDef::Lime       : *this = Raylib::Color(0, 158, 47, 255);
+            case ColorDef::DarkGreen  : *this = Raylib::Color(0, 117, 44, 255);
+            case ColorDef::SkyBlue    : *this = Raylib::Color(102, 191, 255, 255);
+            case ColorDef::Blue       : *this = Raylib::Color(0, 121, 241, 255);
+            case ColorDef::DarkBlue   : *this = Raylib::Color(0, 82, 172, 255);
+            case ColorDef::Purple     : *this = Raylib::Color(200, 122, 255, 255);
+            case ColorDef::Violet     : *this = Raylib::Color(135, 60, 190, 255);
+            case ColorDef::DarkPurple : *this = Raylib::Color(112, 31, 126, 255);
+            case ColorDef::Beige      : *this = Raylib::Color(211, 176, 131, 255);
+            case ColorDef::Brown      : *this = Raylib::Color(127, 106, 79, 255);
+            case ColorDef::DarkBrown  : *this = Raylib::Color(76, 63, 47, 255);
+            case ColorDef::White      : *this = Raylib::Color(255, 255, 255, 255);
+            case ColorDef::Black      : *this = Raylib::Color(0, 0, 0, 255);
+            case ColorDef::Blank      : *this = Raylib::Color(0, 0, 0, 0);
+            case ColorDef::Magenta    : *this = Raylib::Color(255, 0, 255, 255);
+            case ColorDef::RayWhite   : *this = Raylib::Color(245, 245, 245, 255);
+            default                : *this = Raylib::Color(0, 0, 0, 0);
         }
-        return Raylib::Color(0, 0, 0, 0);
+    }
+
+    Color &Color::operator=(const Color& color)
+    {
+        this->r = color.r;
+        this->g = color.g;
+        this->b = color.b;
+        this->a = color.a;
+        return *this;
     }
 }
